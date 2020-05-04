d8d22d9dc6062dae20869c87e5f4ba46e3a0579e
ametaz <ametaz.mn@gmail.com>
2020-05-04 11:45:47 +0000

    1srt angular assignement

---
+++ src/app/alerts/alerts.component.ts
@@ -0,0 +1,19 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-alert',
+    templateUrl: 'alerts.component.html',
+  styles: [`
+  li
+  {
+      display: inline;
+  }
+  p{
+      font-size: x-large;
+  }
+  `]
+})
+export class AlertComponent
+{
+
+}
